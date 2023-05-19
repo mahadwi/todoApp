5e238dd609a468f489c3a3d129bab3c9220ef662
mahadwiputra008 <mahadwiputra008@gmail.com>
2023-05-20 06:09:51 +0700

    menambahkan local storage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const todos = [];
 const RENDER_EVENT = 'render-todo';
+const SAVED_EVENT = 'saved-todo';
+const STORAGE_KEY = 'TODO_APPS';
 
 document.addEventListener('DOMContentLoaded', function(){
     const submitForm = document.getElementById('form');
@@ -7,7 +9,19 @@ document.addEventListener('DOMContentLoaded', function(){
         event.preventDefault();
         addTodo();
     });
+
+    if(isStorageExist()){
+        loadDataFromStorage();
+    }
 });
+
+function isStorageExist() {
+    if(typeof(Storage) === undefined){
+        alert('Browser kamu tidak mendukung local storage');
+        return false;
+    }
+    return true;
+}
 
 function addTodo() {
     const textTodo = document.getElementById('title').value;
@@ -18,6 +32,7 @@ function addTodo() {
     todos.push(todoObject);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function generateId(){
@@ -86,6 +101,7 @@ function addTaskCompleted(todoId){
 
     todoTarget.isCompleted = true;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findTodo(todoId){
@@ -104,6 +120,7 @@ function removeTaskFromCompleted(todoId){
 
     todos.splice(todoTarget, 1);
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function undoTaskCompleted(todoId){
@@ -113,6 +130,7 @@ function undoTaskCompleted(todoId){
 
     todoTarget.isCompleted = false;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findTodoIndex(todoId){
@@ -123,6 +141,27 @@ function findTodoIndex(todoId){
     }
 
     return -1;
+}
+
+function saveData() {
+    if(isStorageExist()){
+        const parsed = JSON.stringify(todos);
+        localStorage.setItem(STORAGE_KEY, parsed);
+        document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+}
+
+function loadDataFromStorage(){
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+
+    if(data !== null){
+        for(const todo of data){
+            todos.push(todo);
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
 document.addEventListener(RENDER_EVENT, function(){
@@ -141,3 +180,7 @@ document.addEventListener(RENDER_EVENT, function(){
         }
     }
 });
+
+document.addEventListener(SAVED_EVENT, function(){
+    console.log(localStorage.getItem(STORAGE_KEY));
+})
